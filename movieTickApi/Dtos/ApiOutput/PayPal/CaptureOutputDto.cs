8bc70b06de6f2af0aeee383f92f748ac7fe9093f
yang <exvincse@gmail.com>
2025-03-17 17:13:20 +0800

    新增paypal回傳dto

---
+++ movieTickApi/Dtos/ApiOutput/PayPal/CaptureOutputDto.cs
@@ -0,0 +1,10 @@
+﻿namespace movieTickApi.Dtos.ApiOutput.PayPal
+{
+        public class CaptureOutputDto
+        {
+                public required string OrderId { get; set; }
+                public required string Status { get; set; }
+                public required string Amount { get; set; }
+                public DateTime CreateTime { get; set; }
+        }
+}
